f5417b9a68b429bea9ec386ef7ae1f5b410e5d7a
richard-fagot <richard.fagot@gmail.com>
2014-01-21 16:33:27 +0100

    add the google map api.

---
+++ project_env.bat
@@ -0,0 +1,3 @@
+set PLAY_HOME=C:\RFA\Temporaire\Projets\COLLAT\dev\play-2.2.1
+set JAVA_HOME=C:\_Java_\jdk1.7.0_45
+set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;C:\Program Files\Intel\DMIX;C:\Program Files\Fichiers communs\Roxio Shared\DLLShared\;C:\WINDOWS\system32\WindowsPowerShell\v1.0;C:\Program Files\ATI Technologies\ATI.ACE\Core-Static;%GNU_HOME%\bin;%MYSQL_HOME%\bin;%PLAY_HOME%;%JAVA_HOME%\bin
