a4a1361fdead608ce342c4f5bf1178dce81e6724
richard-fagot <richard.fagot@gmail.com>
2014-01-22 18:09:53 +0100

    some stuff

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -43,8 +43,34 @@ public class Application extends Controller {
 	
 	@BodyParser.Of(BodyParser.Json.class)
 	public static Result saveDistance() {
+//		{
+//			"rows":[
+//				{"elements":[
+//					{"distance":{"text":"78,4 km","value":78407},
+//					 "duration":{"text":"1 heure 10 min","value":4200},
+//					 "status":"OK"},
+//					{"distance":{"text":"135 km","value":135237},
+//					 "duration":{"text":"1 heure 56 min","value":6982},
+//					 "status":"OK"}
+//				]}
+//			],
+//			"originAddresses":["Impasse Alice Guy, 31300 Toulouse, France"],
+//			"destinationAddresses":["Saint-Clar, France","Galiax, France"]
+//		}
 		JsonNode json = request().body().asJson();
-		
+		JsonNode dests = json.get("destinationAddresses");
+		for(JsonNode row : json.get("rows")) {
+			JsonNode destsData = row.get("elements");
+			for(int i = 0 ; i < dests.size() ; i++) {
+				JsonNode destData = destsData.get(i);
+				String asText = dests.get(i).asText();
+				String city = asText.substring(0, asText.indexOf(","))+", Gers";
+				Distance distance = Distance.find.where().ilike("destination", city+"%").findUnique();
+				distance.setDuration(destData.get("duration").get("value").asDouble());
+				distance.setDistance(destData.get("distance").get("value").asDouble());
+				distance.save();
+			}
+		}
 		return ok();
 	}
 	
